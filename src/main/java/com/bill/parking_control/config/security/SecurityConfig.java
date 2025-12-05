92c5a3231bd628798703aea6e2ddff1760f9a3f8
antoniocariocajr <antoniocariocajr@gmail.com>
2025-12-05 06:20:03 -0300

    chore: Update SpringDoc OpenAPI dependency, remove explicit security ignore for API docs, and add port to MongoDB URI.

--- src/main/java/com/bill/parking_control/config/security/SecurityConfig.java
+++ src/main/java/com/bill/parking_control/config/security/SecurityConfig.java
@@ -13,7 +13,6 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -74,8 +73,4 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring().requestMatchers("/v3/api-docs/**");
-    }
 }
